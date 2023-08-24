00eb6af5b7b98cb7daa5498f9c3801d96e619b79
Alex H <alex.holder@bigfootds.com>
2023-08-24 20:00:06 +1000

    feat: our first throw!

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ let userEnteredNumber = false;
 do {
 	userInput = promptInstance("What is the coolest number in the universe?");
 
+	if (!parseInt(userInput) && !parseFloat(userInput)){
+		//throw "User did not enter a number";
+		throw new Error("User did not enter a number.");
+	}
+
+
 	let userInputAsNumber = Number(userInput);
 
 	if (userInputAsNumber === 42){
