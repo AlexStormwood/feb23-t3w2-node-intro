abf54b07bfd363fb42f4579479fffd63a4a022c4
Alex H <alex.holder@bigfootds.com>
2023-08-24 21:09:52 +1000

    feat: custom error, try-catch

--- index.js
+++ index.js
@@ -1,33 +1,88 @@
 //const promptInstance = require('prompt-sync')({sigint:true});
 
 const promptSync = require('prompt-sync');
-const promptInstance = promptSync({
-	sigint: true // allows CTRL+C to exit the app
-});
 
-// some comment here
-// comment for naomi
 
-let userInput = null;
-let userEnteredNumber = false;
-do {
-	userInput = promptInstance("What is the coolest number in the universe?");
 
-	if (!parseInt(userInput) && !parseFloat(userInput)){
-		//throw "User did not enter a number";
-		throw new Error("User did not enter a number.");
+class SomeCustomError extends Error {
+	constructor(message = "Some error happened, write a better message lol"){
+		super();
+
+		this.statusCode = 404;
+
+		this.message = message;
 	}
+}
 
 
-	let userInputAsNumber = Number(userInput);
+function app(){
+	const promptInstance = promptSync({
+		sigint: true // allows CTRL+C to exit the app
+	});
+	
+	// some comment here
+	// comment for naomi
+	
+	let userInput = null;
+	let userEnteredNumber = false;
+	do {
+		userInput = promptInstance("What is the coolest number in the universe?");
+	
+		if (!parseInt(userInput) && !parseFloat(userInput)){
+			//throw "User did not enter a number";
+			throw new SomeCustomError();
+		}
+	
+	
+		let userInputAsNumber = Number(userInput);
+	
+		if (userInputAsNumber === 42){
+			console.log("The universe thanks you for figuring out the answer!!");
+			userEnteredNumber = true;
+		} else {
+			console.log("The universe seems unfulfilled...");
+		}	
+	} while (!userEnteredNumber);
+}
 
-	if (userInputAsNumber === 42){
-		console.log("The universe thanks you for figuring out the answer!!");
-		userEnteredNumber = true;
+
+try {
+	app();
+	console.log("app executed successfully");
+} catch(error) {
+
+	/*
+  if (!(e instanceof Error)) {
+    e = new Error(e);
+  }
+	*/
+
+	// switch (string(typeof(error))){
+	// 	case "SomeCustomError"
+	// }
+
+
+	if (error.statusCode){
+		console.log(`Error ${error.statusCode} occured, its message is: ${error.message}`);
 	} else {
-		console.log("The universe seems unfulfilled...");
-	}	
-} while (!userEnteredNumber);
+		console.log("an error occurred! " + error.message);
+	}
+} finally {
+	console.log("App has finished running. Pretend there's nice DB closing code here.")
+}
+
+
+/*
+
+try {
+	some code to execute
+} catch (some Error instance) {
+	some error-handling code
+} finally {
+	close up smoothly and safely without instantly crashing or otherwise quickly closing 
+}
+
+*/
 
 
 // helloWorld(userInput);
